ac28aadfcb59996cbb5c89976de34800ae4c1ac2
Kamil Samigullin <kamil@samigullin.info>
2017-11-03 16:29:50 +0300

    prepare backbone

---
+++ cmd/web/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Get("/{UID}", func(rw http.ResponseWriter, req *http.Request) {
+		rw.Write([]byte("get form schema"))
+	})
+	r.Post("/{UID}", func(rw http.ResponseWriter, req *http.Request) {
+		rw.Write([]byte("send form data"))
+	})
+	log.Fatal(http.ListenAndServe(":"+port(), r))
+}
+
+func port() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	return port
+}

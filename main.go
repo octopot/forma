1a6a8e3db7eb49ab1aae2e8886dbf6013b0aa668
Kamil Samigullin <kamil@samigullin.info>
2017-12-20 15:56:41 +0300

    prepare main.go for test

--- main.go
+++ main.go
@@ -2,23 +2,42 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"io"
+	"os"
 	"runtime"
 
 	"github.com/kamilsk/form-api/cmd"
 	"github.com/spf13/cobra"
 )
 
-func main() {
+const (
+	success = 0
+	failed  = 1
+)
+
+func main() { application{Stderr: os.Stderr, Stdout: os.Stdout, Shutdown: os.Exit}.Run() }
+
+type application struct {
+	Stderr, Stdout io.Writer
+	Shutdown       func(code int)
+}
+
+// Run executes the application logic.
+func (app application) Run() {
 	cmd.RootCmd.AddCommand(&cobra.Command{
 		Use:   "version",
 		Short: "Show application version",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("Version %s (commit: %s, build date: %s, go version: %s, compiler: %s, platform: %s)\n",
+			fmt.Fprintf(app.Stderr,
+				"Version %s (commit: %s, build date: %s, go version: %s, compiler: %s, platform: %s)\n",
 				version, commit, date, runtime.Version(), runtime.Compiler, runtime.GOOS+"/"+runtime.GOARCH)
 		},
 	})
 	if err := cmd.RootCmd.Execute(); err != nil {
-		log.Fatal(err)
+		fmt.Fprintln(app.Stderr, err)
+		app.Shutdown(failed)
+		return
 	}
+	app.Shutdown(success)
+	return
 }

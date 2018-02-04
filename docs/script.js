e9a0c16cb5030341e574e390f251056a66841523
Kamil Samigullin <kamil@samigullin.info>
2018-02-04 21:48:06 +0300

    fix bug in docs/script.js

--- docs/script.js
+++ docs/script.js
@@ -55,7 +55,7 @@
 
     $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
         const target = $(e.target).attr('href').substr(1);
-        $('pre[data-target="' + target + '"').each(function (i, node) {
+        $('pre[data-target="' + target + '"]').each(function (i, node) {
             $(node).removeClass('d-none');
             $(node).siblings('pre').addClass('d-none');
         });

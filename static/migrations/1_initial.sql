d49954db2fe23cf7412ecef57bb558af11917c54
Kamil Samigullin <kamil@samigullin.info>
2018-03-26 20:07:19 +0300

    update deps, format 1_initial.sql

--- static/migrations/1_initial.sql
+++ static/migrations/1_initial.sql
@@ -3,38 +3,43 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- +migrate StatementBegin
-CREATE FUNCTION update_timestamp() RETURNS TRIGGER AS $update_timestamp$
+CREATE FUNCTION update_timestamp()
+  RETURNS TRIGGER AS $update_timestamp$
 BEGIN
   NEW.updated_at := current_timestamp;
   RETURN NEW;
--- [42883] ERROR: could not identify an equality operator for type xml
---   IF NEW.* IS DISTINCT FROM OLD.* THEN
---     NEW.updated_at := current_timestamp;
---     RETURN NEW;
---   ELSE
---     RETURN OLD;
---   END IF;
+  -- [42883] ERROR: could not identify an equality operator for type xml
+  --   IF NEW.* IS DISTINCT FROM OLD.* THEN
+  --     NEW.updated_at := current_timestamp;
+  --     RETURN NEW;
+  --   ELSE
+  --     RETURN OLD;
+  --   END IF;
 END;
-$update_timestamp$ LANGUAGE plpgsql;
+$update_timestamp$
+LANGUAGE plpgsql;
 -- +migrate StatementEnd
 
-CREATE TYPE STATUS AS ENUM ('enabled','disabled');
+CREATE TYPE STATUS AS ENUM ('enabled', 'disabled');
 
 CREATE TABLE "form_schema" (
-  "uuid" UUID NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
-  "user" UUID NOT NULL,
-  "schema" XML NOT NULL,
-  "status" STATUS NOT NULL DEFAULT 'enabled',
-  "created_at" TIMESTAMP NOT NULL DEFAULT now(),
+  "uuid"       UUID      NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+  "user"       UUID      NOT NULL,
+  "schema"     XML       NOT NULL,
+  "status"     STATUS    NOT NULL             DEFAULT 'enabled',
+  "created_at" TIMESTAMP NOT NULL             DEFAULT now(),
   "updated_at" TIMESTAMP
 );
 
-CREATE TRIGGER "form_data_updated" BEFORE UPDATE ON "form_schema" FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
+CREATE TRIGGER "form_data_updated"
+  BEFORE UPDATE
+  ON "form_schema"
+  FOR EACH ROW EXECUTE PROCEDURE update_timestamp();
 
 CREATE TABLE "form_data" (
-  "id" SERIAL PRIMARY KEY,
-  "uuid" UUID NOT NULL,
-  "data" JSONB NOT NULL,
+  "id"         SERIAL PRIMARY KEY,
+  "uuid"       UUID      NOT NULL,
+  "data"       JSONB     NOT NULL,
   "created_at" TIMESTAMP NOT NULL DEFAULT now()
 );
 
@@ -42,7 +47,8 @@ CREATE TABLE "form_data" (
 
 DROP TABLE "form_data";
 
-DROP TRIGGER "form_data_updated" ON "form_schema";
+DROP TRIGGER "form_data_updated"
+ON "form_schema";
 
 DROP TABLE "form_schema";
 

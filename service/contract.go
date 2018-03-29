0883c72ea14fd2a184cc3876fbda6413ed565880
Kamil Samigullin <kamil@samigullin.info>
2018-03-29 12:56:02 +0300

    remove arg names from interfaces

--- service/contract.go
+++ service/contract.go
@@ -5,7 +5,7 @@ import "github.com/kamilsk/form-api/domain"
 // Storage defines the behavior of Data Access Object.
 type Storage interface {
 	// AddData inserts form data and returns their ID.
-	AddData(uuid domain.UUID, values map[string][]string) (int64, error)
+	AddData(domain.UUID, map[string][]string) (int64, error)
 	// Schema returns the form schema with provided UUID.
-	Schema(uuid domain.UUID) (domain.Schema, error)
+	Schema(domain.UUID) (domain.Schema, error)
 }

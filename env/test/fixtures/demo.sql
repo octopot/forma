204902a241717bbdb6808f5c7376da18592740e1
Kamil Samigullin <kamil@samigullin.info>
2018-07-19 14:20:17 +0300

    fix #128: update demo migration

--- env/test/fixtures/demo.sql
+++ env/test/fixtures/demo.sql
@@ -1,62 +1,131 @@
 DO $$
-DECLARE   demoUser "form_schema"."user"%TYPE := '00000000-0000-4000-8000-000000000000';
-  DECLARE email    "form_data"."uuid"%TYPE := '41ca5e09-3ce2-4094-b108-3ecc257c6fa4';
-  DECLARE githubEn "form_data"."uuid"%TYPE := '41059676-6cb6-42bc-9fb5-8df1699b6881';
-  DECLARE githubRu "form_data"."uuid"%TYPE := '41d33b3d-26fe-435c-9402-c8e4a1f87cc1';
+DECLARE   demoAccount  "account"."id"%TYPE := '10000000-2000-4000-8000-160000000001';
+  DECLARE demoUser     "user"."id"%TYPE := '10000000-2000-4000-8000-160000000002';
+  DECLARE demoToken    "token"."id"%TYPE := '10000000-2000-4000-8000-160000000003';
+  DECLARE subscribe    "schema"."id"%TYPE := '10000000-2000-4000-8000-160000000004';
+  DECLARE feedbackEn   "schema"."id"%TYPE := '10000000-2000-4000-8000-160000000005';
+  DECLARE feedbackRu   "schema"."id"%TYPE := '10000000-2000-4000-8000-160000000006';
+  DECLARE subscribeTpl "template"."id"%TYPE := '10000000-2000-4000-8000-160000000007';
+  DECLARE feedbackTpl  "template"."id"%TYPE := '10000000-2000-4000-8000-160000000008';
 BEGIN
-  DELETE FROM "form_data"
-  WHERE "uuid" IN (email, githubEn, githubRu);
+  DELETE FROM "log"
+  WHERE "account_id" = demoAccount
+        OR "schema_id" IN (subscribe, feedbackEn, feedbackRu)
+        OR "template_id" IN (subscribeTpl, feedbackTpl);
 
-  DELETE FROM "form_schema"
-  WHERE "uuid" IN (email, githubEn, githubRu);
+  DELETE FROM "input"
+  WHERE "schema_id" IN (subscribe, feedbackEn, feedbackRu);
 
-  INSERT INTO "form_schema" ("uuid", "user", "schema")
+  DELETE FROM "template"
+  WHERE "account_id" = demoAccount OR "id" IN (subscribeTpl, feedbackTpl);
+
+  DELETE FROM "schema"
+  WHERE "account_id" = demoAccount OR "id" IN (subscribe, feedbackEn, feedbackRu);
+
+  DELETE FROM "token"
+  WHERE "id" = demoToken OR "user_id" = demoUser;
+
+  DELETE FROM "user"
+  WHERE "id" = demoUser OR "account_id" = demoAccount;
+
+  DELETE FROM "account"
+  WHERE "id" = demoAccount;
+
+  INSERT INTO "account" ("id", "name")
+  VALUES (demoAccount, 'Demo account');
+
+  INSERT INTO "user" ("id", "account_id", "name")
+  VALUES (demoUser, demoAccount, 'Demo user');
+
+  INSERT INTO "token" ("id", "user_id", "expired_at")
+  VALUES (demoToken, demoUser, NULL);
+
+  INSERT INTO "schema" ("id", "account_id", "content")
   VALUES
-    (email, demoUser, '
+    (subscribe, demoAccount, '
     <form lang="en" title="Email subscription" action="https://kamil.samigullin.info/" method="post"
           enctype="application/x-www-form-urlencoded">
         <input name="email" type="email" title="Email" maxlength="64" required="1"/>
-    </form>
-    ' :: XML),
-    (githubEn, demoUser, '
+    </form>'),
+    (feedbackEn, demoAccount, '
     <form lang="en" title="GitHub demo page" action="https://kamilsk.github.io/form-api/" method="post"
           enctype="application/x-www-form-urlencoded">
         <input name="name" type="text" title="Name" placeholder="Name..." maxlength="25" required="1"/>
         <input name="feedback" type="text" title="Feedback" placeholder="Your feedback..." maxlength="255"
                required="1"/>
-    </form>
-    ' :: XML),
-    (githubRu, demoUser, '
+    </form>'),
+    (feedbackRu, demoAccount, '
     <form lang="ru" title="GitHub демо" action="https://kamilsk.github.io/form-api/" method="post"
           enctype="application/x-www-form-urlencoded">
         <input name="name" type="text" title="Имя" placeholder="Имя..." maxlength="25" required="1"/>
         <input name="feedback" type="text" title="Комментарий" placeholder="Ваш комментарий..." maxlength="255"
                required="1"/>
-    </form>
-    ' :: XML);
+    </form>');
 
-  INSERT INTO "form_data" ("uuid", "data")
+  INSERT INTO "template" ("id", "account_id", "content")
   VALUES
-    (email, '{
-      "email": [
-        "test@my.email"
-      ]
-    }'),
-    (githubEn, '{
-      "name": [
-        "C. Northcote Parkinson"
-      ],
-      "feedback": [
-        "Work contracts to fit in the time we give it."
-      ]
-    }'),
-    (githubRu, '{
-      "name": [
-        "Сирил Норткот Паркинсон"
-      ],
-      "feedback": [
-        "Работа заполняет время, отпущенное на неё."
-      ]
-    }');
+    (subscribeTpl, demoAccount, '{{- define "forma.body" -}}
+    <div class="row">
+        {{- with .Schema.Input "email" -}}
+            <div class="col-md-8">
+                <span class="bmd-form-group">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="material-icons">mail</i>
+                            </span>
+                        </div>
+                        {{- template "forma.input" . -}}
+                    </div>
+                </span>
+            </div>
+        {{- end -}}
+    </div>
+{{- end -}}
+{{- define "forma.submit" -}}
+    {{- template "forma.powered_by" . -}}
+    <div class="col-md-4">
+        <button class="btn btn-primary btn-block" type="submit">Subscribe</button>
+    </div>
+{{- end -}}'),
+    (feedbackTpl, demoAccount, '{{- define "forma.body" -}}
+    {{- with .Schema.Input "name" -}}
+        <div class="form-group row">
+            <label for="{{ .ID }}"
+                   class="col-sm-3 col-form-label">{{ .Name }}</label>
+            <div class="col-sm-9">
+                {{- template "forma.input" . -}}
+            </div>
+        </div>
+    {{- end -}}
+    {{- with .Schema.Input "feedback" -}}
+        <div class="form-group row">
+            <label for="{{ .ID }}"
+                   class="col-sm-3 col-form-label">{{ .Name }}</label>
+            <div class="col-sm-9">
+                {{- template "forma.input" . -}}
+            </div>
+        </div>
+    {{- end -}}
+{{- end -}}
+{{- define "forma.submit" -}}
+    <input name="_redirect" type="hidden" value="https://kamilsk.github.io/form-api/">
+    <input name="_timeout" type="hidden" value="60">
+    <input class="btn btn-dark" type="submit">
+{{- end -}}');
+
+  INSERT INTO "input" ("schema_id", "data")
+  VALUES
+    (subscribe, '{
+      "email": ["test@my.email"]
+    }' :: JSONB),
+    (feedbackEn, '{
+      "name": ["C. Northcote Parkinson"],
+      "feedback": ["Work contracts to fit in the time we give it."]
+    }' :: JSONB),
+    (feedbackRu, '{
+      "name": ["Сирил Норткот Паркинсон"],
+      "feedback": ["Работа заполняет время, отпущенное на неё."]
+    }' :: JSONB);
 END;
 $$;

3c6bb4202028285304ce2d8531e81ab5091ceae0
Kamil Samigullin <kamil@samigullin.info>
2018-07-21 17:36:38 +0300

    fix #144: add Secret type

---
+++ pkg/config/secret_test.go
@@ -0,0 +1,48 @@
+package config_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/kamilsk/form-api/pkg/config"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v2"
+)
+
+func TestSecret_Printing(t *testing.T) {
+	type password struct {
+		Value config.Secret `yaml:"password" json:"password"`
+	}
+	secret := password{Value: "secret"}
+
+	testCases := []struct {
+		name    string
+		marshal func(password) ([]byte, error)
+	}{
+		{"print by `%#v`", func(pass password) ([]byte, error) {
+			str := fmt.Sprintf("%#v", pass)
+			return []byte(str), nil
+		}},
+		{"print by `%s`", func(pass password) ([]byte, error) {
+			str := fmt.Sprintf("%s", pass.Value)
+			return []byte(str), nil
+		}},
+		{"json marshal", func(pass password) ([]byte, error) {
+			return json.Marshal(pass)
+		}},
+		{"yaml marshal", func(pass password) ([]byte, error) {
+			return yaml.Marshal(pass)
+		}},
+	}
+
+	for _, test := range testCases {
+		tc := test
+		t.Run(test.name, func(t *testing.T) {
+			raw, err := tc.marshal(secret)
+			assert.NoError(t, err)
+			assert.False(t, strings.Contains(string(raw), string(secret.Value)))
+		})
+	}
+}

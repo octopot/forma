dfad884d51d55f7253e8d361173f32a1f7651e37
Kamil Samigullin <kamil@samigullin.info>
2018-08-21 20:09:08 +0300

    add first storage test

--- pkg/storage/storage_test.go
+++ pkg/storage/storage_test.go
@@ -1,1 +1,18 @@
 package storage_test
+
+import (
+	"testing"
+
+	"github.com/kamilsk/form-api/pkg/errors"
+	"github.com/kamilsk/form-api/pkg/storage"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMust(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		assert.NotPanics(t, func() { storage.Must(func(*storage.Storage) error { return nil }) })
+	})
+	t.Run("panic", func(t *testing.T) {
+		assert.Panics(t, func() { storage.Must(func(*storage.Storage) error { return errors.Simple("test") }) })
+	})
+}

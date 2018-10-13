810bc8e584f5f6268c151e19ab63a4d2abb834eb
Kamil Samigullin <kamil@samigullin.info>
2018-10-13 13:17:25 +0300

    rename log table to event, review its structure

--- pkg/static/migrations/4_audit.sql
+++ pkg/static/migrations/4_audit.sql
@@ -1,27 +1,28 @@
 -- +migrate Up
 
-CREATE TABLE "log" (
+CREATE TABLE "event" (
   "id"          BIGSERIAL PRIMARY KEY,
-  "account_id"  UUID      NOT NULL,
-  "schema_id"   UUID      NOT NULL,
-  "input_id"    UUID      NOT NULL,
-  "template_id" UUID      NULL     DEFAULT NULL,
-  "identifier"  UUID      NOT NULL,
-  "code"        INTEGER   NOT NULL,
-  "context"     JSONB     NOT NULL,
-  "created_at"  TIMESTAMP NOT NULL DEFAULT now()
+  "account_id"  UUID          NOT NULL,
+  "schema_id"   UUID          NOT NULL,
+  "input_id"    UUID          NOT NULL,
+  "template_id" UUID          NULL     DEFAULT NULL,
+  "identifier"  UUID          NULL     DEFAULT NULL,
+  "context"     JSONB         NOT NULL,
+  "code"        INTEGER       NOT NULL,
+  "url"         VARCHAR(1024) NOT NULL,
+  "created_at"  TIMESTAMP     NOT NULL DEFAULT now()
 );
 
-CREATE TRIGGER "immutable_log"
+CREATE TRIGGER "immutable_event"
   BEFORE UPDATE
-  ON "log"
+  ON "event"
   FOR EACH ROW EXECUTE PROCEDURE ignore_update();
 
 
 
 -- +migrate Down
 
-DROP TRIGGER "immutable_log"
-ON "log";
+DROP TRIGGER "immutable_event"
+ON "event";
 
-DROP TABLE "log";
+DROP TABLE "event";

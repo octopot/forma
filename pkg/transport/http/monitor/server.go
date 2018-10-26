1001b706900759dd7bfc4a73ebe93cc3034d12fd
Kamil Samigullin <kamil@samigullin.info>
2018-10-26 21:35:36 +0300

    define monitoring server

---
+++ pkg/transport/http/monitor/server.go
@@ -0,0 +1,27 @@
+package monitor
+
+import (
+	"expvar"
+	"net"
+	"net/http"
+
+	"github.com/kamilsk/form-api/pkg/config"
+	"github.com/kamilsk/form-api/pkg/transport"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+// New TODO issue#173
+func New(_ config.MonitoringConfig) transport.Server {
+	return &server{}
+}
+
+type server struct{}
+
+// Serve TODO issue#173
+func (*server) Serve(listener net.Listener) error {
+	defer listener.Close()
+	mux := &http.ServeMux{}
+	mux.Handle("/metrics", promhttp.Handler())
+	mux.Handle("/vars", expvar.Handler())
+	return http.Serve(listener, mux)
+}
